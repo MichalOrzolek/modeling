ef3f49a7cd711b6c3073e0534fb06b5cae71147c
Michał Orzołek <108025976+MichalOrzolek@users.noreply.github.com>
2023-05-17 23:12:50 +0200

    Add files via upload

---
+++ projections_arima.m
@@ -0,0 +1,58 @@
+clear all
+
+% Specify the ARIMA model order
+p = 8; % Autoregressive (AR) order
+d = 1; % Differencing order
+q = 7; % Moving average (MA) order
+train_frac = 0.8;   % 80% of the data is for training
+profit_margin= 0.15; % percentage of revenue withheld as profit
+
+t = readtable('data_matlab_wdays.csv');
+dates = datetime(t.Var1, 'InputFormat', 'dd-MM-yyyy');
+values = t.Var2;
+tt = timetable(dates,values);
+
+% Split data into training and testing sets
+n = height(tt);
+n_train = round(n * train_frac);
+train_tt = tt(1:n_train, :);
+test_tt = tt(n_train+1:end, :); %number of testing samles n+1:end
+
+% Fit the ARIMA model to the training data
+model = arima(p, d, q);
+estModel = estimate(model, train_tt.values);
+
+% Forecast 
+num_periods = numel(test_tt.dates);
+[forecast, forecast_ci] = forecast(estModel, num_periods, 'Y0', train_tt.values);
+
+% Calculate Mean Squared Error (MSE)
+mse = mean((test_tt.values - forecast).^2);
+% Calculate Mean Absolute Error (MAE)
+mae = mean(abs(test_tt.values - forecast));
+
+% Plot the results
+figure
+hold on
+plot(tt.dates, tt.values, 'k', 'LineWidth', 1.5, 'DisplayName', 'Original Data')
+plot(train_tt.dates, train_tt.values, 'b', 'LineWidth', 1.5, 'DisplayName', 'Training Data')
+plot(test_tt.dates, forecast, 'r', 'LineWidth', 1.5, 'DisplayName', 'Forecast')
+legend('show')
+xlabel('Time')
+ylabel('Value')
+title('Time Series Forecasting using ARIMA')
+hold off
+
+%revenue and profit calculation
+revenue = 23*tt.values;
+profit=revenue*profit_margin;
+sales_tt=timetable(tt.dates,revenue,profit);
+monthly_tt = retime(sales_tt, 'monthly', 'sum');
+
+% Create a bar chart of the monthly Revenue and Profit
+figure;
+bar(monthly_tt.Time, [monthly_tt.Var1, monthly_tt.Var2]);
+xlabel('Month');
+ylabel('Amount');
+title('Monthly Revenue and Profit');
+legend({'Revenue', 'Profit'});
