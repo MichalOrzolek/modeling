d63532c92979b4dcb3f927abd1871ad9ee751837
Michał Orzołek <108025976+MichalOrzolek@users.noreply.github.com>
2023-12-19 12:47:52 +0100

    Update correction_script.py

--- Python/correction_script.py
+++ Python/correction_script.py
@@ -71,6 +71,25 @@ def iterate_over_url(url):
         time.sleep(sleeptime)
         return error_value
 
+def merge_function(lowercase_country_list):
+    for element in lowercase_country_list:
+        country = element
+        file_path = f'data/output_{country}.xlsx'
+        print('Current country: ', country)
+        print('Previous country: ', previous_country)
+        if previous_country:
+            previous_file_path = f'data/output_{previous_country}.xlsx'
+            df_current = pd.read_excel(file_path)
+            df_previous = pd.read_excel(previous_file_path)
+            frames = [df_previous, df_current]
+            result = pd.concat(frames)
+            with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+                result.to_excel(writer,sheet_name='Sheet_1', index=False)
+        else:
+            None
+        previous_country = country
+
+
     #___________________CODE______________________#
 
 
@@ -87,28 +106,21 @@ for country in eu_countries_lower:
     file_path = f'data/output_{country}.xlsx'
     print(country)
     df = pd.read_excel(file_path)
-
-
+    
     for index, row in df.iterrows():
-        
         total += 1
         # Check if the 'h index' value is NaN
         if pd.isna(row['H index']):
             # If NaN, call the iterate_over_url function on the 'Citec link'
             number_of_NaN += 1
-    
-    percentage = number_of_NaN/total
-    percentage = round(percentage, 2)
-    print('Number of NaN rows in ' + country + ': ', number_of_NaN, ' (', percentage, ')')
-
-#uncomment the following if want to update:
-'''
             repec_data = iterate_over_url(row['Citec link'])
             print(repec_data)
             for key, value in repec_data:
                 df.at[index, key] = value
-
+    print('Missing rows: ', number_of_NaN)
     file_name = f'data/output_{country}.xlsx'
     with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
             df.to_excel(writer,sheet_name='Sheet_1', index=False)
-'''
+
+#merge all the corrected files into one table for all countries
+merge_function(eu_countries_lower)
